f24cf380756d2d0cda571dda4a39e6d327fb8460
tangcent <sweettom@foxmail.com>
2019-04-21 18:46:30 +0800

    support Export Markdown

---
+++ common-api/src/main/kotlin/com/itangcent/common/utils/KitUtils.kt
@@ -0,0 +1,11 @@
+package com.itangcent.common.utils
+
+object KitUtils {
+
+    fun <T> fromBool(boolean: Boolean, whenTrue: T, whenFalse: T): T {
+        return when (boolean) {
+            true -> whenTrue
+            false -> whenFalse
+        }
+    }
+}
