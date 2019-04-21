4935071f7417bae995339b1fedb9142f1c9370c2
tangcent <sweettom@foxmail.com>
2019-04-21 18:46:30 +0800

    support Export Markdown

---
+++ idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/actions/MarkdownExportAction.kt
@@ -0,0 +1,40 @@
+package com.itangcent.idea.plugin.actions
+
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.Project
+import com.itangcent.idea.plugin.api.export.DocParseHelper
+import com.itangcent.idea.plugin.api.export.markdown.MarkdownApiExporter
+import com.itangcent.idea.plugin.api.export.postman.PostmanApiExporter
+import com.itangcent.idea.plugin.api.export.postman.PostmanConfigReader
+import com.itangcent.intellij.config.ConfigReader
+import com.itangcent.intellij.context.ActionContext
+import com.itangcent.intellij.extend.guice.singleton
+import com.itangcent.intellij.extend.guice.with
+import com.itangcent.intellij.file.DefaultLocalFileRepository
+import com.itangcent.intellij.file.LocalFileRepository
+import com.itangcent.intellij.psi.ClassRuleConfig
+import com.itangcent.intellij.psi.DefaultClassRuleConfig
+import com.itangcent.intellij.setting.ReadOnlySettingManager
+import com.itangcent.intellij.setting.SettingManager
+
+class MarkdownExportAction : ApiExportAction("Export Markdown") {
+
+    override fun onBuildActionContext(builder: ActionContext.ActionContextBuilder) {
+        super.onBuildActionContext(builder)
+
+        builder.bind(LocalFileRepository::class) { it.with(DefaultLocalFileRepository::class).singleton() }
+        builder.bind(SettingManager::class) { it.with(ReadOnlySettingManager::class).singleton() }
+        builder.bind(MarkdownApiExporter::class) { it.singleton() }
+        builder.bind(DocParseHelper::class) { it.singleton() }
+        builder.bind(ClassRuleConfig::class) { it.with(DefaultClassRuleConfig::class).singleton() }
+        builder.bind(ConfigReader::class) { it.with(PostmanConfigReader::class).singleton() }
+
+        builder.bindInstance("file.save.default", "easy-api.md")
+        builder.bindInstance("file.save.last.location.key", "com.itangcent.markdown.export.path")
+    }
+
+    override fun actionPerformed(actionContext: ActionContext, project: Project?, anActionEvent: AnActionEvent) {
+        actionContext.instance(MarkdownApiExporter::class).export()
+    }
+
+}
