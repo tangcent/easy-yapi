7eaf8620b5bbb10f41f1a9d6aefd9f32fa176ffa
tangcent <sweettom@foxmail.com>
2019-05-22 20:53:00 +0800

    support clear cache in Setting

--- idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/settings/Settings.kt
+++ idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/settings/Settings.kt
@@ -13,4 +13,22 @@ class Settings {
         return newSetting
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Settings
+
+        if (pullNewestDataBefore != other.pullNewestDataBefore) return false
+        if (postmanToken != other.postmanToken) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = pullNewestDataBefore?.hashCode() ?: 0
+        result = 31 * result + (postmanToken?.hashCode() ?: 0)
+        return result
+    }
+
 }
