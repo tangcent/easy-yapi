335fb76d7cb6c0014585deaa683eaaa66c256627
tangcent <sweettom@foxmail.com>
2019-05-24 07:43:05 +0800

    deserialize int numbers correctly

--- idea-plugin/src/main/kotlin/com/itangcent/idea/utils/GsonExUtils.kt
+++ idea-plugin/src/main/kotlin/com/itangcent/idea/utils/GsonExUtils.kt
@@ -1,25 +1,72 @@
 package com.itangcent.idea.utils
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import com.google.gson.JsonParser
+import com.google.gson.TypeAdapterFactory
+import com.google.gson.internal.bind.ObjectTypeAdapter
 import com.itangcent.common.utils.GsonUtils
+import com.itangcent.common.utils.RegisterExclusionStrategy
+import com.itangcent.common.utils.Visional
+import com.itangcent.intellij.extend.getPropertyValue
 
 object GsonExUtils {
+
+    private val gson: Gson
+
+    init {
+        val numberObjectTypeAdapter = NumberFixedObjectTypeAdapter()
+        gson = GsonBuilder()
+                .setExclusionStrategies(RegisterExclusionStrategy().exclude(Visional::class.java))
+                .registerTypeAdapterFactory(NumberFixedObjectTypeAdapter.FACTORY)
+                .registerTypeAdapter(Any::class.java, numberObjectTypeAdapter)
+                .registerTypeAdapter(Map::class.java, numberObjectTypeAdapter)
+                .registerTypeAdapter(List::class.java, numberObjectTypeAdapter)
+                .create()
+        numberObjectTypeAdapter.setGson(gson)
+
+        try {
+            formatGson(gson)
+        } catch (e: Exception) {
+            //I am very sad
+            //But there is no plan B to fix it.
+        }
+
+    }
+
+    /**
+     * try remove default ObjectTypeAdapter from gson
+     * if success,the {@link NumberFixedObjectTypeAdapter} will be used instead
+     */
+    @Suppress("UNCHECKED_CAST")
+    private fun formatGson(gson: Gson) {
+
+        var factories = gson.getPropertyValue("factories") ?: return
+
+        //unwrap for UnmodifiableList
+        if (factories::class.qualifiedName == "java.util.Collections.UnmodifiableRandomAccessList" ||
+                factories::class.qualifiedName == "java.util.Collections.UnmodifiableList") {
+            factories = factories.getPropertyValue("list") ?: return
+        }
+
+        (factories as MutableList<TypeAdapterFactory>).remove(ObjectTypeAdapter.FACTORY)
+    }
 
     fun toJson(bean: Any?): String {
         val beanWithClass = BeanWithClass()
         if (bean != null) {
             beanWithClass.c = bean.javaClass.name
-            beanWithClass.j = GsonUtils.toJson(bean)
+            beanWithClass.j = gson.toJson(bean)
         }
-        return GsonUtils.toJson(beanWithClass)
+        return gson.toJson(beanWithClass)
     }
 
     @Suppress("UNCHECKED_CAST")
     fun <T> fromJson(json: String): T? {
-        val beanWithClass = GsonUtils.fromJson(json, BeanWithClass::class)
+        val beanWithClass = gson.fromJson(json, BeanWithClass::class.java)
         if (beanWithClass.c == null) return null
         val cls: Class<T> = Class.forName(beanWithClass.c) as Class<T>
-        return GsonUtils.fromJson(beanWithClass.j!!, cls)
+        return gson.fromJson(beanWithClass.j!!, cls)
     }
 
     class BeanWithClass {
@@ -35,3 +82,4 @@ object GsonExUtils {
         return GsonUtils.prettyJson(jsonObject)
     }
 }
+
