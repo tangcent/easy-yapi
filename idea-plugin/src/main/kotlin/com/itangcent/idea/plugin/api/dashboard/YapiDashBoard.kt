9de85f33be6ef89745cc040766136f4845557607
tangcent <sweettom@foxmail.com>
2019-08-10 12:31:43 +0800

    update toolTip of ApiProjectNode in YapiDashBoard

--- idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/api/dashboard/YapiDashBoard.kt
+++ idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/api/dashboard/YapiDashBoard.kt
@@ -1,10 +1,12 @@
 package com.itangcent.idea.plugin.api.dashboard
 
 import com.google.inject.Inject
+import com.google.inject.Singleton
 import com.itangcent.idea.plugin.dialog.YapiDashboardDialog
 import com.itangcent.intellij.context.ActionContext
 import com.itangcent.intellij.util.UIUtils
 
+@Singleton
 class YapiDashBoard {
 
     @Inject
