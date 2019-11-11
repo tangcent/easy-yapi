ff4d652d34085a1af5a3e90c08c2790974b7302d
tangcent <sweettom@foxmail.com>
2019-11-11 13:12:46 +0800

    support load yaml/yml

--- idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/api/export/EasyApiConfigReader.kt
+++ idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/api/export/EasyApiConfigReader.kt
@@ -1,18 +1,19 @@
 package com.itangcent.idea.plugin.api.export
 
 import com.itangcent.intellij.config.AutoSearchConfigReader
-import com.itangcent.intellij.context.ActionContext
-import com.itangcent.intellij.extend.guice.PostConstruct
-import com.itangcent.intellij.psi.ContextSwitchListener
 
 class EasyApiConfigReader : AutoSearchConfigReader() {
 
-//    @PostConstruct
+    //    @PostConstruct
     fun init() {
         loadConfigInfo()
     }
 
     override fun configFileNames(): List<String> {
-        return listOf(".easy.api.config")
+        return listOf(
+                ".easy.api.config",
+                ".easy.api.yml",
+                ".easy.api.yaml"
+        )
     }
 }
--- idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/api/export/postman/PostmanConfigReader.kt
+++ idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/api/export/postman/PostmanConfigReader.kt
@@ -1,17 +1,23 @@
 package com.itangcent.idea.plugin.api.export.postman
 
 import com.itangcent.intellij.config.AutoSearchConfigReader
-import com.itangcent.intellij.extend.guice.PostConstruct
-import java.util.*
 
 class PostmanConfigReader : AutoSearchConfigReader() {
 
-//    @PostConstruct
+    //    @PostConstruct
     fun init() {
         loadConfigInfo()
     }
 
     override fun configFileNames(): List<String> {
-        return listOf(".postman.config", ".easy.api.config")
+        return listOf(
+                ".postman.config",
+                ".postman.yml",
+                ".postman.yaml",
+
+                ".easy.api.config",
+                ".easy.api.yml",
+                ".easy.api.yaml"
+        )
     }
 }
