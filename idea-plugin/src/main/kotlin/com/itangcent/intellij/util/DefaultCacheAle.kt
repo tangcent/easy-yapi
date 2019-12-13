5f866191e771bed3badcc119b070d9799809ee6c
tangcent <sweettom@foxmail.com>
2019-12-13 23:01:32 +0800

    cache parsed additional `Header`/`Param`

---
+++ idea-plugin/src/main/kotlin/com/itangcent/intellij/util/DefaultCacheAle.kt
@@ -0,0 +1,24 @@
+package com.itangcent.intellij.util
+
+import com.google.inject.Singleton
+import com.itangcent.common.utils.safeComputeIfAbsent
+
+@Singleton
+class DefaultCacheAle : CacheAble {
+
+    val cache: MutableMap<Any, Any> = HashMap()
+
+    override fun <T : Any> cache(key: Any, action: () -> T?): T? {
+        val value = cache.safeComputeIfAbsent(key) {
+            action() ?: NULL
+        }
+        if (value == NULL) {
+            return null
+        }
+        return value as T?
+    }
+
+    companion object {
+        val NULL = Any()
+    }
+}
