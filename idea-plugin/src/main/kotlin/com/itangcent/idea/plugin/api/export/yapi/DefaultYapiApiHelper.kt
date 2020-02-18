b2f12dfc0b4303cd2cda56ceab27902609d2d5ef
tangcent <sweettom@foxmail.com>
2020-02-18 20:21:52 +0800

    refactor `YapiApiHelper`

---
+++ idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/api/export/yapi/DefaultYapiApiHelper.kt
@@ -0,0 +1,187 @@
+package com.itangcent.idea.plugin.api.export.yapi
+
+import com.google.inject.Singleton
+import com.itangcent.common.utils.GsonUtils
+import com.itangcent.common.utils.KV
+import com.itangcent.intellij.extend.asList
+import org.apache.commons.lang3.StringUtils
+import org.apache.commons.lang3.exception.ExceptionUtils
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.entity.ContentType
+import org.apache.http.entity.StringEntity
+import java.util.*
+import kotlin.collections.set
+import kotlin.concurrent.withLock
+
+@Singleton
+open class DefaultYapiApiHelper : AbstractYapiApiHelper(), YapiApiHelper {
+
+    //$projectId$cartName -> $cartId
+    private var cartIdCache: HashMap<String, String> = HashMap()
+
+    override fun findCartWeb(module: String, cartName: String): String? {
+        val token = getPrivateToken(module)
+        val projectId = getProjectIdByToken(token!!) ?: return null
+        val catId = findCat(token, cartName) ?: return null
+        return getCartWeb(projectId, catId)
+    }
+
+    override fun getCartWeb(projectId: String, catId: String): String? {
+        return "$server/project/$projectId/interface/api/cat_$catId"
+    }
+
+    override fun getApiWeb(module: String, cartName: String, apiName: String): String? {
+        val token = getPrivateToken(module)
+        val projectId = getProjectIdByToken(token!!) ?: return null
+        val catId = findCat(token, cartName) ?: return null
+        val apiId = findApi(token, catId, apiName)
+        return "$server/project/$projectId/interface/api/$apiId"
+    }
+
+    override fun findCat(token: String, name: String): String? {
+        val projectId: String? = getProjectIdByToken(token) ?: return null
+        val key = "$projectId$name"
+        var cachedCartId = cacheLock.readLock().withLock { cartIdCache[key] }
+        if (cachedCartId != null) return cachedCartId
+        val cats = getProjectInfo(token, projectId)
+                ?.asJsonObject
+                ?.get("data")
+                ?.asJsonObject
+                ?.get("cat")
+                ?.asJsonArray
+        cats?.forEach { cat ->
+            if (cat.asJsonObject
+                            .get("name")
+                            .asString == name) {
+                cachedCartId = cat.asJsonObject
+                        .get("_id")
+                        .asString
+                if (cachedCartId != null) {
+                    cacheLock.writeLock().withLock {
+                        cartIdCache[key] = cachedCartId!!
+                    }
+                }
+                return cachedCartId
+            }
+        }
+        return null
+    }
+
+    override fun saveApiInfo(apiInfo: HashMap<String, Any?>): Boolean {
+
+        val httpClient = httpClientProvide!!.getHttpClient()
+
+        val httpPost = HttpPost(server + SAVEAPI)
+        beforeCall(httpPost)
+
+        val requestEntity = StringEntity(GsonUtils.toJson(apiInfo),
+                ContentType.APPLICATION_JSON)
+        httpPost.entity = requestEntity
+        val responseHandler = reservedResponseHandle()
+
+        try {
+            val returnValue = httpClient.execute(httpPost, responseHandler).result()
+            val errMsg = findErrorMsg(returnValue)
+            if (StringUtils.isNotBlank(errMsg)) {
+                logger!!.info("Post failed:$errMsg")
+                logger.info("api info:${GsonUtils.toJson(apiInfo)}")
+                return false
+            }
+            return true
+        } catch (e: Throwable) {
+            logger!!.error("Post failed:" + ExceptionUtils.getStackTrace(e))
+            return false
+        }
+    }
+
+    override fun addCart(privateToken: String, name: String, desc: String): Boolean {
+        val projectId = getProjectIdByToken(privateToken) ?: return false
+        return addCart(projectId, privateToken, name, desc)
+    }
+
+    override fun addCart(projectId: String, token: String, name: String, desc: String): Boolean {
+
+        val httpClient = httpClientProvide!!.getHttpClient()
+
+        val httpPost = HttpPost(server + ADDCART)
+        beforeCall(httpPost)
+
+        val requestEntity = StringEntity(GsonUtils.toJson(KV.create<Any?, Any?>()
+                .set("desc", desc)
+                .set("project_id", projectId)
+                .set("name", name)
+                .set("token", token)
+        ),
+                ContentType.APPLICATION_JSON)
+
+        httpPost.entity = requestEntity
+
+        val responseHandler = reservedResponseHandle()
+
+        try {
+            val returnValue = httpClient.execute(httpPost, responseHandler).result()
+            val errMsg = findErrorMsg(returnValue)
+            if (StringUtils.isNotBlank(errMsg)) {
+                logger!!.info("Post failed:$errMsg")
+                return false
+            }
+            val resObj = GsonUtils.parseToJsonTree(returnValue)
+            val addCartId: String? = resObj?.asJsonObject
+                    ?.get("data")
+                    ?.asJsonObject
+                    ?.get("_id")
+                    ?.asString
+            if (addCartId != null) {
+                cacheLock.writeLock().withLock {
+                    cartIdCache["$projectId$name"] = addCartId
+                }
+                logger!!.info("Add new cart:$server/project/$projectId/interface/api/cat_$addCartId")
+            } else {
+                logger!!.info("Add cart failed,response is:$returnValue")
+            }
+            cacheLock.writeLock().withLock { projectInfoCache.remove(projectId) }
+            return true
+        } catch (e: Throwable) {
+            logger!!.error("Post failed:" + ExceptionUtils.getStackTrace(e))
+            return false
+        }
+    }
+
+    override fun findApi(token: String, catId: String, apiName: String): String? {
+        val url = "$server$GETCAT?token=$token&catid=$catId&limit=1000"
+        return GsonUtils.parseToJsonTree(getByApi(url))
+                ?.asJsonObject
+                ?.get("data")
+                ?.asJsonObject
+                ?.get("list")
+                ?.asJsonArray?.firstOrNull { api ->
+            api.asJsonObject.get("title")
+                    .asString == apiName
+        }?.asJsonObject?.get("_id")?.asString
+    }
+
+    override fun findApis(token: String, catId: String): ArrayList<Any?>? {
+        val url = "$server$GETCAT?token=$token&catid=$catId&limit=1000"
+        return GsonUtils.parseToJsonTree(getByApi(url))
+                ?.asJsonObject
+                ?.get("data")
+                ?.asJsonObject
+                ?.get("list")
+                ?.asList()
+    }
+
+    override fun findCarts(project_id: String, token: String): ArrayList<Any?>? {
+        val url = "$server$GETCATMENU?project_id=$project_id&token=$token"
+        return GsonUtils.parseToJsonTree(getByApi(url))
+                ?.asJsonObject
+                ?.get("data")
+                ?.asList()
+    }
+
+    companion object {
+        var ADDCART = "/api/interface/add_cat"
+        const val GETCATMENU = "/api/interface/getCatMenu"
+        const val SAVEAPI = "/api/interface/save"
+        const val GETCAT = "/api/interface/list_cat"
+    }
+}
--- idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/api/export/yapi/YapiApiHelper.kt
+++ idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/api/export/yapi/YapiApiHelper.kt
@@ -1,183 +1,49 @@
 package com.itangcent.idea.plugin.api.export.yapi
 
-import com.itangcent.common.utils.GsonUtils
-import com.itangcent.common.utils.KV
-import com.itangcent.intellij.extend.asList
-import org.apache.commons.lang3.StringUtils
-import org.apache.commons.lang3.exception.ExceptionUtils
-import org.apache.http.client.methods.HttpPost
-import org.apache.http.entity.ContentType
-import org.apache.http.entity.StringEntity
+import com.google.gson.JsonElement
+import com.google.inject.ImplementedBy
 import java.util.*
-import kotlin.collections.set
-import kotlin.concurrent.withLock
+import kotlin.collections.HashMap
 
-open class YapiApiHelper : AbstractYapiApiHelper() {
 
-    //$projectId$cartName -> $cartId
-    private var cartIdCache: HashMap<String, String> = HashMap()
+@ImplementedBy(DefaultYapiApiHelper::class)
+interface YapiApiHelper {
 
-    open fun findCartWeb(module: String, cartName: String): String? {
-        val token = getPrivateToken(module)
-        val projectId = getProjectIdByToken(token!!) ?: return null
-        val catId = findCat(token, cartName) ?: return null
-        return getCartWeb(projectId, catId)
-    }
+    fun findCartWeb(module: String, cartName: String): String?
 
-    open fun getCartWeb(projectId: String, catId: String): String? {
-        return "$server/project/$projectId/interface/api/cat_$catId"
-    }
+    fun getCartWeb(projectId: String, catId: String): String?
 
-    open fun getApiWeb(module: String, cartName: String, apiName: String): String? {
-        val token = getPrivateToken(module)
-        val projectId = getProjectIdByToken(token!!) ?: return null
-        val catId = findCat(token, cartName) ?: return null
-        val apiId = findApi(token, catId, apiName)
-        return "$server/project/$projectId/interface/api/$apiId"
-    }
+    fun getApiWeb(module: String, cartName: String, apiName: String): String?
 
-    open fun findCat(token: String, name: String): String? {
-        val projectId: String? = getProjectIdByToken(token) ?: return null
-        val key = "$projectId$name"
-        var cachedCartId = cacheLock.readLock().withLock { cartIdCache[key] }
-        if (cachedCartId != null) return cachedCartId
-        val cats = getProjectInfo(token, projectId)
-                ?.asJsonObject
-                ?.get("data")
-                ?.asJsonObject
-                ?.get("cat")
-                ?.asJsonArray
-        cats?.forEach { cat ->
-            if (cat.asJsonObject
-                            .get("name")
-                            .asString == name) {
-                cachedCartId = cat.asJsonObject
-                        .get("_id")
-                        .asString
-                if (cachedCartId != null) {
-                    cacheLock.writeLock().withLock {
-                        cartIdCache[key] = cachedCartId!!
-                    }
-                }
-                return cachedCartId
-            }
-        }
-        return null
-    }
+    fun findCat(token: String, name: String): String?
 
-    open fun saveApiInfo(apiInfo: HashMap<String, Any?>): Boolean {
+    fun saveApiInfo(apiInfo: HashMap<String, Any?>): Boolean
 
-        val httpClient = httpClientProvide!!.getHttpClient()
+    fun addCart(privateToken: String, name: String, desc: String): Boolean
 
-        val httpPost = HttpPost(server + SAVEAPI)
+    fun addCart(projectId: String, token: String, name: String, desc: String): Boolean
 
-        val requestEntity = StringEntity(GsonUtils.toJson(apiInfo),
-                ContentType.APPLICATION_JSON)
-        httpPost.entity = requestEntity
-        val responseHandler = reservedResponseHandle()
+    fun findApi(token: String, catId: String, apiName: String): String?
 
-        try {
-            val returnValue = httpClient.execute(httpPost, responseHandler).result()
-            val errMsg = findErrorMsg(returnValue)
-            if (StringUtils.isNotBlank(errMsg)) {
-                logger!!.info("Post failed:$errMsg")
-                logger.info("api info:${GsonUtils.toJson(apiInfo)}")
-                return false
-            }
-            return true
-        } catch (e: Throwable) {
-            logger!!.error("Post failed:" + ExceptionUtils.getStackTrace(e))
-            return false
-        }
-    }
+    fun findApis(token: String, catId: String): ArrayList<Any?>?
 
-    open fun addCart(privateToken: String, name: String, desc: String): Boolean {
-        val projectId = getProjectIdByToken(privateToken) ?: return false
-        return addCart(projectId, privateToken, name, desc)
-    }
+    fun findCarts(project_id: String, token: String): ArrayList<Any?>?
 
-    open fun addCart(projectId: String, token: String, name: String, desc: String): Boolean {
+    fun findServer(): String?
 
-        val httpClient = httpClientProvide!!.getHttpClient()
+    fun setYapiServer(yapiServer: String)
 
-        val httpPost = HttpPost(server + ADDCART)
+    fun setToken(module: String, token: String)
 
-        val requestEntity = StringEntity(GsonUtils.toJson(KV.create<Any?, Any?>()
-                .set("desc", desc)
-                .set("project_id", projectId)
-                .set("name", name)
-                .set("token", token)
-        ),
-                ContentType.APPLICATION_JSON)
+    fun getProjectIdByToken(token: String): String?
 
-        httpPost.entity = requestEntity
+    fun getProjectInfo(token: String, projectId: String?): JsonElement?
 
-        val responseHandler = reservedResponseHandle()
+    fun readTokens(): HashMap<String, String>
 
-        try {
-            val returnValue = httpClient.execute(httpPost, responseHandler).result()
-            val errMsg = findErrorMsg(returnValue)
-            if (StringUtils.isNotBlank(errMsg)) {
-                logger!!.info("Post failed:$errMsg")
-                return false
-            }
-            val resObj = GsonUtils.parseToJsonTree(returnValue)
-            val addCartId: String? = resObj?.asJsonObject
-                    ?.get("data")
-                    ?.asJsonObject
-                    ?.get("_id")
-                    ?.asString
-            if (addCartId != null) {
-                cacheLock.writeLock().withLock {
-                    cartIdCache["$projectId$name"] = addCartId
-                }
-                logger!!.info("Add new cart:$server/project/$projectId/interface/api/cat_$addCartId")
-            } else {
-                logger!!.info("Add cart failed,response is:$returnValue")
-            }
-            cacheLock.writeLock().withLock { projectInfoCache.remove(projectId) }
-            return true
-        } catch (e: Throwable) {
-            logger!!.error("Post failed:" + ExceptionUtils.getStackTrace(e))
-            return false
-        }
-    }
+    fun removeToken(token: String)
 
-    protected open fun findApi(token: String, catId: String, apiName: String): String? {
-        val url = "$server$GETCAT?token=$token&catid=$catId&limit=1000"
-        return GsonUtils.parseToJsonTree(getByApi(url))
-                ?.asJsonObject
-                ?.get("data")
-                ?.asJsonObject
-                ?.get("list")
-                ?.asJsonArray?.firstOrNull { api ->
-            api.asJsonObject.get("title")
-                    .asString == apiName
-        }?.asJsonObject?.get("_id")?.asString
-    }
+    fun removeTokenByModule(module: String)
 
-    open fun findApis(token: String, catId: String): ArrayList<Any?>? {
-        val url = "$server$GETCAT?token=$token&catid=$catId&limit=1000"
-        return GsonUtils.parseToJsonTree(getByApi(url))
-                ?.asJsonObject
-                ?.get("data")
-                ?.asJsonObject
-                ?.get("list")
-                ?.asList()
-    }
-
-    open fun findCarts(project_id: String, token: String): ArrayList<Any?>? {
-        val url = "$server$GETCATMENU?project_id=$project_id&token=$token"
-        return GsonUtils.parseToJsonTree(getByApi(url))
-                ?.asJsonObject
-                ?.get("data")
-                ?.asList()
-    }
-
-    companion object {
-        var ADDCART = "/api/interface/add_cat"
-        const val GETCATMENU = "/api/interface/getCatMenu"
-        const val SAVEAPI = "/api/interface/save"
-        const val GETCAT = "/api/interface/list_cat"
-    }
+    fun getPrivateToken(module: String): String?
 }
