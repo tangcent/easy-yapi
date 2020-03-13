785b4f796a54c1da01ada750d8bf6b059ba74724
tangcent <sweettom@foxmail.com>
2020-03-13 19:53:49 +0800

    recommend config: private_protected_field_only

--- idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/rule/RuleComputorKit.kt
+++ idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/rule/RuleComputorKit.kt
@@ -7,6 +7,8 @@ import com.itangcent.intellij.config.rule.RuleParser
 import com.itangcent.intellij.jvm.element.ExplicitElement
 
 
+@Deprecated(message = "use com.itangcent.intellij.config.rule.computer",
+        replaceWith = ReplaceWith("com.itangcent.intellij.config.rule.computer"))
 fun <T> RuleComputer.computer(ruleKey: RuleKey<T>, explicitElement: ExplicitElement<*>): T? {
     return this.computer(ruleKey, explicitElement, explicitElement.psi())
 }
