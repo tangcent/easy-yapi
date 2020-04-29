e1cba7f4283dc2df017727e49b5f8edf535c0646
tangcent <sweettom@foxmail.com>
2020-04-29 21:47:47 +0800

    polish: add comment for package `com.itangcent.idea.swing`

--- idea-plugin/src/main/kotlin/com/itangcent/idea/swing/Tooltipable.kt
+++ idea-plugin/src/main/kotlin/com/itangcent/idea/swing/Tooltipable.kt
@@ -1,5 +1,19 @@
 package com.itangcent.idea.swing
 
-interface Tooltipable {
+/**
+ * A component which has the text to display in a tool tip.
+ */
+interface ToolTipAble {
+
+    /**
+     * The text to display in a tool tip.
+     * The text displays when the cursor lingers over the component.
+     * If the text is [null],the tool tip is turned off for this component
+     */
     fun toolTip(): String?
 }
+
+/**
+ * For compatible only.
+ */
+typealias Tooltipable = ToolTipAble
