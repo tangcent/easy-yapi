f65b1ab3f18b5251557dafbc88ed5c73273166fe
tangcent <sweettom@foxmail.com>
2020-05-05 23:33:07 +0800

    fix: request  body preview-language in postman  example

--- idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/api/debug/Debugger.kt
+++ idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/api/debug/Debugger.kt
@@ -3,32 +3,20 @@ package com.itangcent.idea.plugin.api.debug
 import com.google.inject.Inject
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
-import com.itangcent.idea.plugin.api.export.ClassExporter
-import com.itangcent.idea.plugin.api.export.RequestHelper
 import com.itangcent.idea.plugin.dialog.DebugDialog
 import com.itangcent.idea.utils.SwingUtils
 import com.itangcent.intellij.constant.EventKey
 import com.itangcent.intellij.context.ActionContext
-import com.itangcent.intellij.logger.Logger
 import com.itangcent.intellij.util.UIUtils
 import java.lang.ref.WeakReference
 
 class Debugger {
 
     @Inject
-    private val logger: Logger? = null
-
-    @Inject
     private val actionContext: ActionContext? = null
 
     @Inject
     private val project: Project? = null
-
-    @Inject
-    private val classExporter: ClassExporter? = null
-
-    @Inject
-    private val requestHelper: RequestHelper? = null
 
     fun showDebugWindow() {
 
