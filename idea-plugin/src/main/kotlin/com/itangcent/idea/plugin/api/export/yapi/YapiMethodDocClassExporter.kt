bf306a119ffbaa725413320a8b046030022b120a
tangcent <sweettom@foxmail.com>
2020-07-26 13:50:16 +0800

    feat: support property `api.tag.delimiter`

--- idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/api/export/yapi/YapiMethodDocClassExporter.kt
+++ idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/api/export/yapi/YapiMethodDocClassExporter.kt
@@ -1,17 +1,23 @@
 package com.itangcent.idea.plugin.api.export.yapi
 
+import com.google.inject.Inject
 import com.intellij.psi.PsiMethod
 import com.itangcent.common.model.MethodDoc
 import com.itangcent.idea.plugin.api.export.DefaultMethodDocClassExporter
+import com.itangcent.intellij.config.ConfigReader
+import org.apache.commons.lang3.StringUtils
 
 class YapiMethodDocClassExporter : DefaultMethodDocClassExporter() {
+
+    @Inject
+    private val configReader: ConfigReader? = null
 
     override fun processCompleted(method: PsiMethod, methodDoc: MethodDoc) {
         super.processCompleted(method, methodDoc)
 
         val tags = ruleComputer!!.computer(YapiClassExportRuleKeys.TAG, method)
         if (!tags.isNullOrBlank()) {
-            methodDoc.setTags(tags.split("\n")
+            methodDoc.setTags(StringUtils.split(tags, configReader!!.first("api.tag.delimiter") ?: ",\n")
                     .map { it.trim() }
                     .filter { it.isNotBlank() })
         }
