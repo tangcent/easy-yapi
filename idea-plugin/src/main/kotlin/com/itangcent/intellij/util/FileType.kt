9b7b2da5d4f557dc7ac11a7b389e45907419d5d8
tangcent <pentatangcent@gmail.com>
2020-11-09 15:54:02 +0800

    scala will not be supported by default (#282)

--- idea-plugin/src/main/kotlin/com/itangcent/intellij/util/FileType.kt
+++ idea-plugin/src/main/kotlin/com/itangcent/intellij/util/FileType.kt
@@ -1,20 +1,33 @@
 package com.itangcent.intellij.util
 
+import com.itangcent.intellij.context.ActionContext
+import com.itangcent.intellij.tip.OnlyOnceInContextTip
+import com.itangcent.intellij.tip.TipsHelper
+
 /**
  * All file types currently supported.
  */
 enum class FileType(private val suffix: String) {
 
     JAVA("java"),
-    KOTLIN("kt"),
-    SCALA("scala");
+    KOTLIN("kt")
+    //, SCALA("scala")
+    ;
 
     fun suffix(): String {
         return suffix
     }
 
     companion object {
+        private val SUPPORT_SUPPORT_TIP = OnlyOnceInContextTip(
+                "For support scala," +
+                        " please download plugin from https://github.com/tangcent/easy-yapi/releases"
+        )
+
         fun acceptable(fileName: String): Boolean {
+            if (fileName.endsWith("scala")) {
+                ActionContext.instance(TipsHelper::class).showTips(SUPPORT_SUPPORT_TIP)
+            }
             return FileType.values().any { fileName.endsWith(it.suffix()) }
         }
     }
