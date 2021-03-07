df7d59c570f0b4a4f35b959f503a39d9757acaf8
tangcent <pentatangcent@gmail.com>
2021-03-07 16:27:56 +0800

    chore: auto release

---
+++ plugin-script/collect_commits.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#
+
+SOURCE="$0"
+while [[ -h "$SOURCE"  ]]; do # resolve $SOURCE until the file is no longer a symlink
+    scriptDir="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ ${SOURCE} != /*  ]] && SOURCE="$scriptDir/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+scriptDir="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
+basedir=${scriptDir%/*}
+echo "baseDir:"${basedir}
+
+#last_tag_commit=`git rev-list --tags --max-count=1`
+#pre_tag=`git describe --tags $last_tag_commit`
+#echo "pre tag:" $pre_tag
+version=`cat ${basedir}/build.gradle | grep -Eo -m1 '[0-9]\.[0-9]\.[0-9]'`
+echo "version:"${version}
+git fetch origin tag v${version}
+revision=`git log refs/tags/v${version} -1 --pretty=%H`
+current_revision=`git rev-parse HEAD`
+echo "current_revision:"${current_revision}
+commits=`git log --pretty=format:"%s" ${current_revision}...${revision}`
+echo "$commits" > $scriptDir/commits.txt
+echo "commits:"
+cat $scriptDir/commits.txt
