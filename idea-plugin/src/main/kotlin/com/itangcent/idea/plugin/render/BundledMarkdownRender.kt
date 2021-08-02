81b5308feee60ec2f190828992673c9ac9d4dd1a
tangcent <pentatangcent@gmail.com>
2021-08-02 11:01:44 +0800

    chore: adapt markdown versions (#543)

--- idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/render/BundledMarkdownRender.kt
+++ idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/render/BundledMarkdownRender.kt
@@ -1,9 +1,7 @@
 package com.itangcent.idea.plugin.render
 
 import com.google.inject.Singleton
-import org.intellij.markdown.flavours.commonmark.CommonMarkFlavourDescriptor
-import org.intellij.markdown.html.HtmlGenerator
-import org.intellij.markdown.parser.MarkdownParser
+import com.itangcent.plugin.adapter.MarkdownHtmlGenerator
 
 /**
  *
@@ -14,18 +12,10 @@ import org.intellij.markdown.parser.MarkdownParser
 @Singleton
 class BundledMarkdownRender : MarkdownRender {
 
-    private val commonMarkFlavourDescriptor = CommonMarkFlavourDescriptor()
-
     override fun render(markdown: String): String? {
         if (markdown.isBlank()) {
             return null
         }
-        val astNode = MarkdownParser(commonMarkFlavourDescriptor).buildMarkdownTreeFromString(markdown)
-        val htmlGenerator = HtmlGenerator(markdown, astNode, commonMarkFlavourDescriptor, false)
-        val generateHtml = htmlGenerator.generateHtml()
-        return generateHtml
-            .removePrefix("<body>")
-            .removeSuffix("</body>")
-            .takeIf { it.isNotBlank() }
+        return MarkdownHtmlGenerator().render(markdown)
     }
 }
---
+++ plugin-adapter/plugin-adapter-markdown/src/main/kotlin/com/itangcent/plugin/adapter/MarkdownHtmlGenerator.kt
@@ -0,0 +1,54 @@
+package com.itangcent.plugin.adapter
+
+import org.intellij.markdown.flavours.commonmark.CommonMarkFlavourDescriptor
+import org.intellij.markdown.html.HtmlGenerator
+import org.intellij.markdown.parser.MarkdownParser
+import kotlin.reflect.full.functions
+
+/**
+ * see https://github.com/JetBrains/markdown
+ */
+class MarkdownHtmlGenerator {
+
+    private val commonMarkFlavourDescriptor = commonMarkFlavourDescriptorBuilder()
+
+    fun render(markdown: String): String? {
+        if (markdown.isBlank()) {
+            return null
+        }
+        val astNode = MarkdownParser(commonMarkFlavourDescriptor).buildMarkdownTreeFromString(markdown)
+        val htmlGenerator = HtmlGenerator(markdown, astNode, commonMarkFlavourDescriptor, false)
+        val generateHtml = generateHtmlCall(htmlGenerator)
+        return generateHtml
+            .removePrefix("<body>")
+            .removeSuffix("</body>")
+            .takeIf { it.isNotBlank() }
+    }
+
+    companion object {
+        val generateHtmlCall: (HtmlGenerator) -> String = findAdaptedGenerateHtmlCall()
+
+        private fun findAdaptedGenerateHtmlCall(): (HtmlGenerator) -> String {
+            val functions = HtmlGenerator::class.functions
+            for (function in functions) {
+                if (function.name == "generateHtml" && function.parameters.size == 1) {
+                    return { function.call(it) as? String ?: "" }
+                }
+            }
+            return { it.generateHtml() }
+        }
+
+        val commonMarkFlavourDescriptorBuilder: () -> CommonMarkFlavourDescriptor =
+            findAdaptedCommonMarkFlavourDescriptor()
+
+        private fun findAdaptedCommonMarkFlavourDescriptor(): () -> CommonMarkFlavourDescriptor {
+            val constructors = CommonMarkFlavourDescriptor::class.constructors
+            for (constructor in constructors) {
+                if (constructor.parameters.isEmpty()) {
+                    return { constructor.call() }
+                }
+            }
+            return { CommonMarkFlavourDescriptor() }
+        }
+    }
+}
