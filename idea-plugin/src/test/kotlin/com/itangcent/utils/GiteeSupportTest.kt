c6d3b5b650da17b47a7c523409e26ba519f7d73b
tangcent <pentatangcent@gmail.com>
2022-04-17 18:49:34 +0800

    fix: set `readTimeout` for read resource by url (#743)

---
+++ idea-plugin/src/test/kotlin/com/itangcent/utils/GiteeSupportTest.kt
@@ -0,0 +1,20 @@
+package com.itangcent.utils
+
+import org.junit.Assert
+import org.junit.jupiter.api.Test
+
+/**
+ * Test case for [GiteeSupport]
+ * 
+ * @author tangcent
+ */
+internal class GiteeSupportTest {
+
+    @Test
+    fun convertUrlFromGithubToGitee() {
+        Assert.assertEquals("https://gitee.com/tangcent/easy-yapi/raw/master/third/markdown.cn.config",
+            GiteeSupport.convertUrlFromGithub("https://raw.githubusercontent.com/tangcent/easy-yapi/master/third/markdown.cn.config"))
+        Assert.assertEquals("https://gitee.com/tangcent/easy-yapi/raw/master/third/swagger.config",
+            GiteeSupport.convertUrlFromGithub("https://raw.githubusercontent.com/tangcent/easy-yapi/master/third/swagger.config"))
+    }
+}
