87d6b0b20aac715a1579d8ad530cb48a257d16ac
tangcent <pentatangcent@gmail.com>
2022-11-02 08:00:18 +0800

    feat: highlight new content in Settings->Recommend (#853)

--- idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/dialog/EasyApiSettingRecommendGUI.kt
+++ idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/dialog/EasyApiSettingRecommendGUI.kt
@@ -1,13 +1,17 @@
 package com.itangcent.idea.plugin.dialog;
 
 import com.intellij.ui.CheckBoxList
+import com.itangcent.common.logger.Log
+import com.itangcent.common.logger.traceError
 import com.itangcent.common.utils.truncate
 import com.itangcent.idea.plugin.configurable.AbstractEasyApiSettingGUI
 import com.itangcent.idea.plugin.settings.Settings
 import com.itangcent.idea.plugin.settings.helper.RecommendConfigLoader
+import java.util.*
 import javax.swing.JComponent
 import javax.swing.JPanel
 import javax.swing.JTextArea
+import javax.swing.text.DefaultHighlighter
 
 class EasyApiSettingRecommendGUI : AbstractEasyApiSettingGUI() {
 
@@ -30,10 +34,10 @@ class EasyApiSettingRecommendGUI : AbstractEasyApiSettingGUI() {
                         ?.replace("\n", "    ")
         }
 
-        this.recommendConfigList!!.setCheckBoxListListener { index, value ->
+        this.recommendConfigList!!.setCheckBoxListListener { index, checked ->
             val code = RecommendConfigLoader[index]
             val settings = this.settingsInstance!!
-            if (value) {
+            if (checked) {
                 settings.recommendConfigs = RecommendConfigLoader.addSelectedConfig(settings.recommendConfigs, code)
             } else {
                 settings.recommendConfigs = RecommendConfigLoader.removeSelectedConfig(settings.recommendConfigs, code)
@@ -54,14 +58,61 @@ class EasyApiSettingRecommendGUI : AbstractEasyApiSettingGUI() {
     }
 
     private fun computePreviewTextArea() {
-        this.previewTextArea!!.text =
-            RecommendConfigLoader.buildRecommendConfig(
-                settingsInstance?.recommendConfigs ?: "",
-                "\n#${"-".repeat(20)}\n"
-            )
+        val old = this.previewTextArea!!.text
+        val new = RecommendConfigLoader.buildRecommendConfig(
+            settingsInstance?.recommendConfigs ?: "",
+            SEPARATOR
+        )
+        this.previewTextArea!!.text = new
+        if (old.isNotEmpty() && new.length > old.length) {
+            try {
+                val oldIntervals = old.intervals()
+                val newIntervals = new.intervals()
+                var range: IntRange? = null
+                var index = 0
+                while (index < oldIntervals.size) {
+                    if (old.subSequence(oldIntervals[index]) != new.subSequence(newIntervals[index])) {
+                        range = newIntervals[index]
+                        break
+                    }
+                    ++index
+                }
+                if (range == null) {
+                    range = newIntervals.last()
+                }
+                this.previewTextArea!!.highlighter.addHighlight(
+                    range.first,
+                    range.last + 1,
+                    DefaultHighlighter.DefaultPainter
+                )
+            } catch (e: Exception) {
+                LOG.traceError("failed", e)
+            }
+        }
+    }
+
+    private fun String.intervals(): List<IntRange> {
+        val ret = LinkedList<IntRange>()
+        var start = 0
+        do {
+            val next = this.indexOf(SEPARATOR, start)
+            if (next == -1) {
+                break
+            }
+            ret += start until next
+            start = next + SEPARATOR.length
+        } while (start < this.length)
+        if (start < this.length) {
+            ret += start until this.length
+        }
+        return ret
     }
 
     override fun readSettings(settings: Settings) {
         settings.recommendConfigs = settingsInstance?.recommendConfigs ?: ""
+    }
+
+    companion object : Log() {
+        var SEPARATOR = "\n#${"-".repeat(20)}\n"
     }
 }
