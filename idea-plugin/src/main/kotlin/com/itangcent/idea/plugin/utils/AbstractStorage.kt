850caf7a0033fcbd177f7960abd9379c2a3d1779
tangcent <pentatangcent@gmail.com>
2023-08-18 08:32:42 +0800

    fix: fix issue with SessionStorage not works (#1016)

--- idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/utils/AbstractStorage.kt
+++ idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/utils/AbstractStorage.kt
@@ -1,7 +1,12 @@
 package com.itangcent.idea.plugin.utils
 
 import com.itangcent.idea.plugin.utils.Storage.Companion.DEFAULT_GROUP
+import com.itangcent.idea.plugin.utils.Storage.Companion.NULL
+import java.util.*
 
+/**
+ * Abstract implementation of [Storage]
+ */
 abstract class AbstractStorage : Storage {
 
     override fun get(name: String?): Any? {
@@ -36,4 +41,93 @@ abstract class AbstractStorage : Storage {
         clear(DEFAULT_GROUP)
     }
 
+    override fun get(group: String?, name: String?): Any? {
+        return getCache(group ?: DEFAULT_GROUP)[name]
+    }
+
+    override fun set(group: String?, name: String?, value: Any?) {
+        useCache(group) { put(name ?: NULL, value) }
+    }
+
+    override fun pop(group: String?, name: String?): Any? {
+        return useQueue(group, name) { pollLast() }
+    }
+
+    override fun peek(group: String?, name: String?): Any? {
+        return useQueue(group, name) { peekLast() }
+    }
+
+    override fun push(group: String?, name: String?, value: Any?) {
+        useQueue(group, name) { addLast(value) }
+    }
+
+    override fun remove(group: String?, name: String) {
+        useCache(group) { remove(name) }
+    }
+
+    override fun keys(group: String?): Array<Any?> {
+        return getCache(group ?: DEFAULT_GROUP).keys.toTypedArray()
+    }
+
+    override fun clear(group: String?) {
+        useCache(group ?: DEFAULT_GROUP) { clear() }
+    }
+
+    /**
+     * Use cache and update cache
+     */
+    @Synchronized
+    private fun <T : Any> useCache(group: String?, action: MutableMap<String, Any?>.() -> T?): T? {
+        val cache = getCache(group ?: DEFAULT_GROUP)
+        try {
+            return cache.action()
+        } finally {
+            onUpdate(group, cache)
+        }
+    }
+
+    /**
+     * Use queue and update cache
+     */
+    @Synchronized
+    @Suppress("UNCHECKED_CAST")
+    private fun <T : Any> useQueue(group: String?, name: String?, action: LinkedList<Any?>.() -> T?): T? {
+        val cache = getCache(group ?: DEFAULT_GROUP)
+        var queue = cache[name ?: NULL]
+        try {
+            when (queue) {
+
+                is LinkedList<*> -> {
+                    return action(queue as LinkedList<Any?>)
+                }
+
+                is Collection<*> -> {
+                    queue = LinkedList<Any?>(queue)
+                    cache[name ?: NULL] = queue
+                    return action(queue)
+                }
+
+                else -> {
+                    queue = LinkedList<Any?>()
+                    cache[name ?: NULL] = queue
+                    return action(queue)
+                }
+            }
+        } finally {
+            if (queue is Collection<*> && queue.isEmpty()) {
+                cache.remove(name ?: NULL)
+            }
+            onUpdate(group, cache)
+        }
+    }
+
+    /**
+     * Get cache of specified group
+     */
+    protected abstract fun getCache(group: String): MutableMap<String, Any?>
+
+    /**
+     * called when cache is updated
+     */
+    protected abstract fun onUpdate(group: String?, cache: MutableMap<String, Any?>)
 }
--- idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/utils/LocalStorage.kt
+++ idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/utils/LocalStorage.kt
@@ -7,8 +7,11 @@ import com.itangcent.common.utils.KV
 import com.itangcent.idea.binder.DbBeanBinderFactory
 import com.itangcent.idea.plugin.utils.Storage.Companion.DEFAULT_GROUP
 import com.itangcent.intellij.file.LocalFileRepository
-import java.util.*
 
+/**
+ * Implementation of [Storage] based on local file
+ * The [LocalStorage] can be accessed cross projects.
+ */
 @Singleton
 @ScriptTypeName("localStorage")
 class LocalStorage : AbstractStorage() {
@@ -16,115 +19,24 @@ class LocalStorage : AbstractStorage() {
     @Inject
     private val localFileRepository: LocalFileRepository? = null
 
-    private var dbBeanBinderFactory: DbBeanBinderFactory<KV<Any?, Any?>>? = null
-
-    private fun getDbBeanBinderFactory(): DbBeanBinderFactory<KV<Any?, Any?>> {
-        if (dbBeanBinderFactory == null) {
-            synchronized(this) {
-                dbBeanBinderFactory = DbBeanBinderFactory(localFileRepository!!.getOrCreateFile(".api.local.storage.v1.1.db").path)
-                { KV.create<Any?, Any?>() }
-            }
-        }
-        return this.dbBeanBinderFactory!!
-    }
-
-    override fun get(group: String?, name: String?): Any? {
-        return getDbBeanBinderFactory().getBeanBinder(group ?: DEFAULT_GROUP)
-                .tryRead()?.get(name)
-    }
-
-    override fun set(group: String?, name: String?, value: Any?) {
-        val beanBinder = getDbBeanBinderFactory().getBeanBinder(group ?: DEFAULT_GROUP)
-        val kv = beanBinder.read()
-        if (value == null) {
-            kv.remove(name)
-        } else {
-            kv[name] = value
-        }
-        beanBinder.save(kv)
-    }
-
-    override fun pop(group: String?, name: String?): Any? {
-        val beanBinder = getDbBeanBinderFactory().getBeanBinder(group ?: DEFAULT_GROUP)
-        val kv = beanBinder.read()
-        return tryQueue(kv, name)?.let {
-            val last = it.pollLast()
-            beanBinder.save(kv)
-            last
-        }
-    }
-
-    override fun peek(group: String?, name: String?): Any? {
-        val beanBinder = getDbBeanBinderFactory().getBeanBinder(group ?: DEFAULT_GROUP)
-        val kv = beanBinder.read()
-        return tryQueue(kv, name)?.peekLast()
-    }
-
-    override fun push(group: String?, name: String?, value: Any?) {
-        val beanBinder = getDbBeanBinderFactory().getBeanBinder(group ?: DEFAULT_GROUP)
-        val kv = beanBinder.read()
-        val queue = queue(kv, name)
-        queue.addLast(value)
-        beanBinder.save(kv)
-    }
-
-    @Suppress("UNCHECKED_CAST")
-    private fun queue(kv: KV<Any?, Any?>, name: String?): LinkedList<Any?> {
-        var queue = kv[name]
-        return when (queue) {
-            is LinkedList<*> -> {
-                queue as LinkedList<Any?>
-            }
-            //convert to linkedList
-            is List<*> -> {
-                val list = LinkedList(queue)
-                kv[name] = list
-                list
-            }
-            else -> {
-                queue = LinkedList<Any>()
-                kv[name ?: Storage.NULL] = queue
-                queue as LinkedList<Any?>
-            }
-        }
-    }
-
-
-    @Suppress("UNCHECKED_CAST")
-    private fun tryQueue(kv: KV<Any?, Any?>, name: String?): LinkedList<Any?>? {
-        val v = kv[name] ?: return null
-        return when (v) {
-            is LinkedList<*> -> {
-                v as LinkedList<Any?>
-            }
-            //convert to linkedList
-            is List<*> -> {
-                val list = LinkedList(v)
-                kv[name] = list
-                list
-            }
-            else -> null
-        }
-    }
-
-
-    override fun remove(group: String?, name: String) {
-        val beanBinder = getDbBeanBinderFactory().getBeanBinder(group ?: DEFAULT_GROUP)
-        val kv = beanBinder.tryRead() ?: return
-        kv.remove(name)
-        beanBinder.save(kv)
-    }
-
-    override fun keys(group: String?): Array<Any?> {
-        return getDbBeanBinderFactory().getBeanBinder(group ?: DEFAULT_GROUP)
-                .tryRead()
-                ?.keys
-                ?.toTypedArray()
-                ?: emptyArray()
+    private val dbBeanBinderFactory: DbBeanBinderFactory<KV<String, Any?>> by lazy {
+        DbBeanBinderFactory(localFileRepository!!.getOrCreateFile(".api.local.storage.v1.1.db").path)
+        { KV.create() }
     }
 
     override fun clear(group: String?) {
-        getDbBeanBinderFactory().deleteBinder(group ?: DEFAULT_GROUP)
+        dbBeanBinderFactory.deleteBinder(group ?: DEFAULT_GROUP)
     }
 
+    override fun getCache(group: String): MutableMap<String, Any?> {
+        return dbBeanBinderFactory.getBeanBinder(group).tryRead() ?: KV.create()
+    }
+
+    override fun onUpdate(group: String?, cache: MutableMap<String, Any?>) {
+        if (cache.isEmpty()) {
+            dbBeanBinderFactory.deleteBinder(group ?: DEFAULT_GROUP)
+        } else {
+            dbBeanBinderFactory.getBeanBinder(group ?: DEFAULT_GROUP).save(cache as KV<String, Any?>)
+        }
+    }
 }
