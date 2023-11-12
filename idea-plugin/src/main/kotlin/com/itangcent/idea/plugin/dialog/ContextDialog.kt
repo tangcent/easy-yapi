51cfe10351788ce775fcabe91e90fdaf285519dd
tangcent <pentatangcent@gmail.com>
2023-11-12 12:25:33 +0800

    feat: Add dialog size memory feature (#1069)

--- idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/dialog/ContextDialog.kt
+++ idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/dialog/ContextDialog.kt
@@ -1,6 +1,8 @@
 package com.itangcent.idea.plugin.dialog
 
 import com.google.inject.Inject
+import com.intellij.ide.util.PropertiesComponent
+import com.itangcent.common.logger.Log
 import com.itangcent.common.logger.traceError
 import com.itangcent.common.utils.safe
 import com.itangcent.idea.utils.initAfterShown
@@ -8,12 +10,19 @@ import com.itangcent.intellij.constant.EventKey
 import com.itangcent.intellij.context.ActionContext
 import com.itangcent.intellij.extend.guice.PostConstruct
 import com.itangcent.intellij.logger.Logger
+import java.awt.Dimension
 import java.awt.event.WindowAdapter
 import java.awt.event.WindowEvent
 import javax.swing.JDialog
 import javax.swing.WindowConstants
 
 abstract class ContextDialog : JDialog() {
+    companion object : Log() {
+        private const val LAST_SIZE = "com.itangcent.easyapi.suv.last.size"
+    }
+
+    private val lastSizePropertiesName
+        get() = "$LAST_SIZE.${this::class.qualifiedName}"
 
     @Inject
     protected lateinit var actionContext: ActionContext
@@ -36,6 +45,8 @@ abstract class ContextDialog : JDialog() {
             }
         })
 
+        restoreSize()
+
         this.initAfterShown {
             try {
                 init()
@@ -44,6 +55,22 @@ abstract class ContextDialog : JDialog() {
                 onCancel()
             } finally {
                 init = true
+            }
+
+            //restore size
+            restoreSize()
+        }
+    }
+
+    private fun restoreSize() {
+        PropertiesComponent.getInstance().getValue(lastSizePropertiesName)?.let {
+            val split = it.split(",")
+            if (split.size == 2) {
+                val width = split[0].toIntOrNull()
+                val height = split[1].toIntOrNull()
+                if (width != null && height != null) {
+                    this.size = Dimension(width, height)
+                }
             }
         }
     }
@@ -87,12 +114,22 @@ abstract class ContextDialog : JDialog() {
         if (!disposed) {
             disposed = true
             dispose()
-            safe { onDispose() }
+            actionContext.runAsync {
+                safe {
+                    PropertiesComponent.getInstance().setValue(
+                        lastSizePropertiesName,
+                        "${this.size.width},${this.size.height}"
+                    )
+                }
+                safe { onDispose() }
+                safe {
+                    actionContext.unHold()
+                    actionContext.stop()
+                }
+            }
         }
     }
 
     open fun onDispose() {
-        actionContext.unHold()
-        actionContext.stop()
     }
 }
