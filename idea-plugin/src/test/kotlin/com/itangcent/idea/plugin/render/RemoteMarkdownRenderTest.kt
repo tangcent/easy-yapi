31b4d0c2db466f1933229bf3866f389875566d04
tangcent <pentatangcent@gmail.com>
2024-06-29 09:59:28 +0800

    test: add test case to verify rendering of non-English content in BundledMarkdownRender (#1142)

--- idea-plugin/src/test/kotlin/com/itangcent/idea/plugin/render/RemoteMarkdownRenderTest.kt
+++ idea-plugin/src/test/kotlin/com/itangcent/idea/plugin/render/RemoteMarkdownRenderTest.kt
@@ -31,192 +31,210 @@ internal open class RemoteMarkdownRenderTest : AdvancedContextTest() {
     override fun customConfig(): String {
         return "markdown.render.server=http://www.itangcent.com/render"
     }
+}
 
-    internal class NoConfigRemoteMarkdownRenderTest : RemoteMarkdownRenderTest() {
+internal class NoConfigRemoteMarkdownRenderTest : RemoteMarkdownRenderTest() {
 
-        override fun bind(builder: ActionContext.ActionContextBuilder) {
-            super.bind(builder)
-            builder.bind(HttpClientProvider::class) {
-                it.toInstance(HttpClientProviderMockBuilder.builder()
+    override fun bind(builder: ActionContext.ActionContextBuilder) {
+        super.bind(builder)
+        builder.bind(HttpClientProvider::class) {
+            it.toInstance(
+                HttpClientProviderMockBuilder.builder()
                     .url("http://www.itangcent.com/render")
-                    .response(content = "<h1>Header</h1>",
-                        contentType = ContentType.TEXT_PLAIN).build())
-            }
-        }
-
-        override fun customConfig(): String {
-            return ""
-        }
-
-        @Test
-        fun testRender() {
-            assertNull(markdownRender.render(""))
-            assertNull(markdownRender.render("\t"))
-
-            @Language("Markdown")
-            val md = "# Header"
-            val html = markdownRender.render(md)
-            assertNull(html)
-        }
-    }
-
-    internal class SuccessRemoteMarkdownRenderTest : RemoteMarkdownRenderTest() {
-
-        override fun bind(builder: ActionContext.ActionContextBuilder) {
-            super.bind(builder)
-            builder.bind(HttpClientProvider::class) {
-                it.toInstance(HttpClientProviderMockBuilder.builder()
-                    .url("http://www.itangcent.com/render")
-                    .response(content = "<h1>Header</h1>",
-                        contentType = ContentType.TEXT_PLAIN).build())
-            }
-        }
-
-        @Test
-        fun testRender() {
-            assertNull(markdownRender.render(""))
-            assertNull(markdownRender.render("\t"))
-
-            @Language("Markdown")
-            val md = "# Header"
-            val html = markdownRender.render(md)
-            assertEquals(
-                "<h1>Header</h1>", html
+                    .response(
+                        content = "<h1>Header</h1>",
+                        contentType = ContentType.TEXT_PLAIN
+                    ).build()
             )
         }
     }
 
-    internal class BadResponseRemoteMarkdownRenderTest : RemoteMarkdownRenderTest() {
+    override fun customConfig(): String {
+        return ""
+    }
 
-        override fun bind(builder: ActionContext.ActionContextBuilder) {
-            super.bind(builder)
-            builder.bind(HttpClientProvider::class) {
-                it.toInstance(HttpClientProviderMockBuilder.builder()
+    @Test
+    fun testRender() {
+        assertNull(markdownRender.render(""))
+        assertNull(markdownRender.render("\t"))
+
+        @Language("Markdown")
+        val md = "# Header"
+        val html = markdownRender.render(md)
+        assertNull(html)
+    }
+}
+
+internal class SuccessRemoteMarkdownRenderTest : RemoteMarkdownRenderTest() {
+
+    override fun bind(builder: ActionContext.ActionContextBuilder) {
+        super.bind(builder)
+        builder.bind(HttpClientProvider::class) {
+            it.toInstance(
+                HttpClientProviderMockBuilder.builder()
                     .url("http://www.itangcent.com/render")
-                    .response(responseCode = 404).build())
-            }
-        }
-
-        @Test
-        fun testRender() {
-            assertNull(markdownRender.render(""))
-            assertNull(markdownRender.render("\t"))
-
-            @Language("Markdown")
-            val md = "# Header"
-            assertNull(markdownRender.render(md))
+                    .response(
+                        content = "<h1>Header</h1>",
+                        contentType = ContentType.TEXT_PLAIN
+                    ).build()
+            )
         }
     }
 
-    internal class FailedRemoteMarkdownRenderTest : RemoteMarkdownRenderTest() {
+    @Test
+    fun testRender() {
+        assertNull(markdownRender.render(""))
+        assertNull(markdownRender.render("\t"))
 
-        override fun bind(builder: ActionContext.ActionContextBuilder) {
-            super.bind(builder)
-            builder.bind(HttpClientProvider::class) {
-                it.toInstance(HttpClientProviderMockBuilder.builder()
+        @Language("Markdown")
+        val md = "# Header"
+        val html = markdownRender.render(md)
+        assertEquals(
+            "<h1>Header</h1>", html
+        )
+    }
+}
+
+internal class BadResponseRemoteMarkdownRenderTest : RemoteMarkdownRenderTest() {
+
+    override fun bind(builder: ActionContext.ActionContextBuilder) {
+        super.bind(builder)
+        builder.bind(HttpClientProvider::class) {
+            it.toInstance(
+                HttpClientProviderMockBuilder.builder()
+                    .url("http://www.itangcent.com/render")
+                    .response(responseCode = 404).build()
+            )
+        }
+    }
+
+    @Test
+    fun testRender() {
+        assertNull(markdownRender.render(""))
+        assertNull(markdownRender.render("\t"))
+
+        @Language("Markdown")
+        val md = "# Header"
+        assertNull(markdownRender.render(md))
+    }
+}
+
+internal class FailedRemoteMarkdownRenderTest : RemoteMarkdownRenderTest() {
+
+    override fun bind(builder: ActionContext.ActionContextBuilder) {
+        super.bind(builder)
+        builder.bind(HttpClientProvider::class) {
+            it.toInstance(
+                HttpClientProviderMockBuilder.builder()
                     .url("http://www.itangcent.com/render")
                     .failed(IllegalArgumentException())
                     .build()
-                )
-            }
-        }
-
-        @Test
-        fun testRender() {
-            assertNull(markdownRender.render(""))
-            assertNull(markdownRender.render("\t"))
-
-            @Language("Markdown")
-            val md = "# Header"
-            assertNull(markdownRender.render(md))
+            )
         }
     }
 
-    internal class OutburstRemoteMarkdownRenderTest : RemoteMarkdownRenderTest() {
+    @Test
+    fun testRender() {
+        assertNull(markdownRender.render(""))
+        assertNull(markdownRender.render("\t"))
 
-        override fun bind(builder: ActionContext.ActionContextBuilder) {
-            super.bind(builder)
-            builder.bind(HttpClientProvider::class) {
-                it.toInstance(HttpClientProviderMockBuilder.builder()
+        @Language("Markdown")
+        val md = "# Header"
+        assertNull(markdownRender.render(md))
+    }
+}
+
+internal class OutburstRemoteMarkdownRenderTest : RemoteMarkdownRenderTest() {
+
+    override fun bind(builder: ActionContext.ActionContextBuilder) {
+        super.bind(builder)
+        builder.bind(HttpClientProvider::class) {
+            it.toInstance(
+                HttpClientProviderMockBuilder.builder()
                     .url("http://www.itangcent.com/render")
-                    .response(content = "<h1>Header</h1>",
+                    .response(
+                        content = "<h1>Header</h1>",
                         contentType = ContentType.TEXT_PLAIN,
-                        elapse = 3000)
+                        elapse = 3000
+                    )
                     .currentTotalLimit(4, IllegalArgumentException("limited!"))
-                    .build())
-            }
-        }
-
-        @Test
-        fun testRender() {
-            assertNull(markdownRender.render(""))
-            assertNull(markdownRender.render("\t"))
-
-            val countDownLatch = CountDownLatch(7)
-            val cyclicBarrier = CyclicBarrier(7)
-            val failed = AtomicInteger()
-
-            @Language("Markdown")
-            val md = "# Header"
-            for (i in 1..7) {
-                Thread {
-                    try {
-                        cyclicBarrier.await()
-                        val html = markdownRender.render(md)
-                        if (html == null) {
-                            failed.incrementAndGet()
-                        } else {
-                            assertEquals(
-                                "<h1>Header</h1>", html
-                            )
-                        }
-                    } finally {
-                        countDownLatch.countDown()
-                    }
-                }.start()
-            }
-            countDownLatch.await()
-            assertEquals(3, failed.get())
+                    .build()
+            )
         }
     }
 
-    internal class SafeRemoteMarkdownRenderTest : RemoteMarkdownRenderTest() {
+    @Test
+    fun testRender() {
+        assertNull(markdownRender.render(""))
+        assertNull(markdownRender.render("\t"))
 
-        override fun bind(builder: ActionContext.ActionContextBuilder) {
-            super.bind(builder)
-            builder.bind(HttpClientProvider::class) {
-                it.toInstance(HttpClientProviderMockBuilder.builder()
-                    .url("http://www.itangcent.com/render")
-                    .response(content = "<h1>Header</h1>",
-                        contentType = ContentType.TEXT_PLAIN,
-                        elapse = 3000)
-                    .currentTotalLimit(7, IllegalArgumentException("limited!"))
-                    .build())
-            }
-        }
+        val countDownLatch = CountDownLatch(7)
+        val cyclicBarrier = CyclicBarrier(7)
+        val failed = AtomicInteger()
 
-        @Test
-        fun testRender() {
-            assertNull(markdownRender.render(""))
-            assertNull(markdownRender.render("\t"))
-
-            val countDownLatch = CountDownLatch(10)
-            for (i in 0..10) {
-                Thread {
-                    try {
-                        @Language("Markdown")
-                        val md = "# Header"
-                        val html = markdownRender.render(md)
+        @Language("Markdown")
+        val md = "# Header"
+        for (i in 1..7) {
+            Thread {
+                try {
+                    cyclicBarrier.await()
+                    val html = markdownRender.render(md)
+                    if (html == null) {
+                        failed.incrementAndGet()
+                    } else {
                         assertEquals(
                             "<h1>Header</h1>", html
                         )
-                    } finally {
-                        countDownLatch.countDown()
                     }
-                }.start()
-            }
-            countDownLatch.await()
+                } finally {
+                    countDownLatch.countDown()
+                }
+            }.start()
+        }
+        countDownLatch.await()
+        assertEquals(3, failed.get())
+    }
+}
+
+internal class SafeRemoteMarkdownRenderTest : RemoteMarkdownRenderTest() {
+
+    override fun bind(builder: ActionContext.ActionContextBuilder) {
+        super.bind(builder)
+        builder.bind(HttpClientProvider::class) {
+            it.toInstance(
+                HttpClientProviderMockBuilder.builder()
+                    .url("http://www.itangcent.com/render")
+                    .response(
+                        content = "<h1>Header</h1>",
+                        contentType = ContentType.TEXT_PLAIN,
+                        elapse = 3000
+                    )
+                    .currentTotalLimit(7, IllegalArgumentException("limited!"))
+                    .build()
+            )
         }
     }
 
+    @Test
+    fun testRender() {
+        assertNull(markdownRender.render(""))
+        assertNull(markdownRender.render("\t"))
+
+        val countDownLatch = CountDownLatch(10)
+        for (i in 0..10) {
+            Thread {
+                try {
+                    @Language("Markdown")
+                    val md = "# Header"
+                    val html = markdownRender.render(md)
+                    assertEquals(
+                        "<h1>Header</h1>", html
+                    )
+                } finally {
+                    countDownLatch.countDown()
+                }
+            }.start()
+        }
+        countDownLatch.await()
+    }
 }
