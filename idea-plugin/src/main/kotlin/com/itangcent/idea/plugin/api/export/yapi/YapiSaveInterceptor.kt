d658c04f294f7c5d2f05b9d695ae119e87b755ba
tangcent <pentatangcent@gmail.com>
2025-04-08 21:06:56 +0800

    feat: support skipping unchanged API updates to YAPI

--- idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/api/export/yapi/YapiSaveInterceptor.kt
+++ idea-plugin/src/main/kotlin/com/itangcent/idea/plugin/api/export/yapi/YapiSaveInterceptor.kt
@@ -1,9 +1,12 @@
 package com.itangcent.idea.plugin.api.export.yapi
 
+import com.google.gson.JsonObject
+import com.google.inject.Inject
 import com.google.inject.ProvidedBy
 import com.google.inject.Singleton
 import com.intellij.openapi.ui.Messages
 import com.itangcent.common.concurrent.ValueHolder
+import com.itangcent.common.utils.anyIsNullOrEmpty
 import com.itangcent.common.utils.toBool
 import com.itangcent.idea.plugin.condition.ConditionOnSetting
 import com.itangcent.idea.plugin.dialog.ConfirmationDialogLabels
@@ -11,6 +14,8 @@ import com.itangcent.idea.swing.MessagesHelper
 import com.itangcent.intellij.config.ConfigReader
 import com.itangcent.intellij.context.ActionContext
 import com.itangcent.intellij.extend.sub
+import com.itangcent.intellij.extend.unbox
+import com.itangcent.intellij.logger.Logger
 import com.itangcent.spi.SpiCompositeBeanProvider
 
 /**
@@ -137,3 +142,87 @@ class NoUpdateDescriptionYapiSaveInterceptor : YapiSaveInterceptor {
         apiInfo["markdown"] = existedMarkdown
     }
 }
+
+/**
+ * A [YapiSaveInterceptor] that prevents updating the API if its content hasn't changed.
+ * This helps reduce unnecessary API calls to YAPI server.
+ */
+@ConditionOnSetting("yapiExportMode", havingValue = "UPDATE_IF_CHANGED")
+class UpdateIfChangedYapiSaveInterceptor : YapiSaveInterceptor {
+    @Inject
+    private lateinit var logger: Logger
+
+    override fun beforeSaveApi(apiHelper: YapiApiHelper, apiInfo: HashMap<String, Any?>): Boolean? {
+        if (!apiHelper.existed(apiInfo)) {
+            return true
+        }
+
+        val existedApi = apiHelper.findExistApi(apiInfo) ?: return true
+        val apiId = existedApi.sub("_id")?.asString ?: return true
+        val existedApiInfo = apiHelper.getApiInfo(apiInfo["token"] as String, apiId) ?: return true
+
+        // Compare key fields that determine if the API has changed
+        if (isApiUnchanged(apiInfo, existedApiInfo)) {
+            logger.info("Skip updating API [${apiInfo["title"]}] as no changes detected")
+            return false
+        }
+        return true
+    }
+
+    // Compare essential fields that determine API changes
+    private val fieldsToCompare = listOf(
+        "title",
+        "desc",
+        "req_body_type",
+        "res_body_type",
+        "req_body_other",
+        "res_body",
+        "req_headers",
+        "req_query",
+        "req_params"
+    )
+
+    private val elementFieldsToCompare = listOf("name", "value", "required", "desc")
+
+    private fun isApiUnchanged(newApi: HashMap<String, Any?>, oldApi: JsonObject): Boolean {
+        return fieldsToCompare.all { field ->
+            val newValue = newApi[field]
+            val oldValue = oldApi[field]?.unbox()
+            isValueEqual(newValue, oldValue)
+        }
+    }
+
+    private fun isValueEqual(value1: Any?, value2: Any?): Boolean {
+        if (value1.anyIsNullOrEmpty() && value2.anyIsNullOrEmpty()) return true
+        if (value1.anyIsNullOrEmpty() || value2.anyIsNullOrEmpty()) {
+            return false
+        }
+
+        return when {
+            value1 is String && value2 is String -> value1 == value2
+            value1 is List<*> && value2 is List<*> -> {
+                if (value1.size != value2.size) {
+                    false
+                } else {
+                    value1.zip(value2).all { (newElement, oldElement) ->
+                        when {
+                            newElement is Map<*, *> && oldElement is Map<*, *> -> {
+                                elementFieldsToCompare.all { field ->
+                                    val newFieldValue = newElement[field]
+                                    val oldFieldValue = oldElement[field]
+                                    isValueEqual(newFieldValue, oldFieldValue)
+                                }
+                            }
+
+                            else -> isValueEqual(newElement, oldElement)
+                        }
+                    }
+                }
+            }
+
+            else -> value1.toString() == value2.toString()
+        }
+    }
+}
+
+val UPDATE_IF_CHANGED_API_SAVE_INTERCEPTOR = UpdateIfChangedYapiSaveInterceptor()
